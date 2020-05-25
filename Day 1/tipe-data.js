c8b4654ed222fc492fe5b217a5b8f808329dc4a0
abuabdirohman4 <kakaabu354@yahoo.com>
2020-05-25 08:05:11 +0700

    Day 1 Done

---
+++ Day 1/tipe-data.js
@@ -0,0 +1,29 @@
+// 1. Memeriksa Tipe Data
+
+console.log(typeof 2020)
+console.log(typeof "Abu Abdirohman")
+console.log(typeof true)
+
+// 2. Komentar
+
+// Ini hanya komentar tidak dieksekusi
+console.log('ini dieksekusi')
+
+/**
+ * conversi jam ke menit
+ * param jam: number
+ */
+function hourToMinutes(hour) {
+    return hour * 60
+}
+
+// 3. Variable
+
+var nama = "Asrul Harahap"
+console.log(nama)
+
+var alamat = "Jakarta Selatan"
+let website = "http://asrul.dev"
+const phi = 3.14
+
+
